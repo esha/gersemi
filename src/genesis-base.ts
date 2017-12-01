a8456615049b82c00439302bf9376f19245c375a
Nathan Bubna <nathan@esha.com>
2017-12-01 10:11:01 -0800

    use live demo API, drop the 'Service' suffix, and use TS-friendly Posterior config structure

--- src/genesis-base.ts
+++ src/genesis-base.ts
@@ -2,25 +2,31 @@ import Posterior from 'posterior';
 import store from 'store2';
 import * as xml2js from 'xml2js';
 
-const defaultConfig = { soap: true, mock: false };
+const defaultConfig = {
+  soap: true,
+  baseURL: 'http://eshademo.cloudapp.net',
+};
 const config = store.get('demo.config', defaultConfig);
-export const GenesisService = Posterior({
-  url: config.mock
-    ? 'http://localhost:8000'
-    : 'http://eshademo.cloudapp.net/soap/FoodQueryService.svc',
+export const Genesis = Posterior({
+  url: config.baseURL,
+  method: config.soap ? 'POST' : 'GET',
   json: !config.soap,
   headers: config.soap
     ? {
         'Content-Type': 'text/xml',
       }
     : {},
+  Children: {
+    Query: {
+      url: '/query',
+    },
+    Edit: {},
+  },
 });
 
 // Support console REPL w/flag
 if (window && document.body.hasAttribute('debug')) {
-  (window as any).GenesisService = GenesisService;
+  (window as any).Genesis = Genesis;
   (window as any).store = store;
   (window as any).xml2js = xml2js;
 }
-
-export default GenesisService;
