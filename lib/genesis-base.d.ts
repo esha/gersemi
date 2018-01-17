9017ab5f36eaf4d379224e85c4d5ff8af12b181f
Nathan Bubna <nathan@esha.com>
2018-01-16 16:35:12 -0800

    not sure why i'm tracking this in git, but update them anyway

--- lib/genesis-base.d.ts
+++ lib/genesis-base.d.ts
@@ -1,2 +1,10 @@
 import Posterior from 'posterior';
-export declare const Genesis: Posterior.Requester;
+export declare class GenesisClient {
+    Base: Posterior.Requester;
+    Service: Posterior.Requester;
+    config: Posterior.InputConfig;
+    constructor(baseConfig: {
+        [key: string]: any;
+    });
+    private configure(key, value, override?);
+}
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,5 @@
+import * as SOAP from './SOAP';
+import * as WSDL from './WSDL';
+import * as XML from './XML';
 export * from './genesis-base';
+export { XML, SOAP, WSDL };
