b7fc601aa939c997ed4588b4c98ac6d86163d6bc
Nathan Bubna <nathan@esha.com>
2018-01-18 16:51:16 -0800

    include all files in index test

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -1,5 +1,15 @@
 import * as index from '../src/index';
 
-test('Should have GenesisBase available', () => {
-  expect(index.GenesisBase).toBeTruthy();
+test('Should have GenesisClient available', () => {
+  expect(index.GenesisClient).toBeTruthy();
+});
+
+test('Should have XML available', () => {
+  expect(index.XML).toBeTruthy();
+});
+test('Should have SOAP available', () => {
+  expect(index.SOAP).toBeTruthy();
+});
+test('Should have WSDL available', () => {
+  expect(index.WSDL).toBeTruthy();
 });
