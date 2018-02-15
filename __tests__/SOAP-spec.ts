77c151040a5f3f54e3fe5983385ab0092d13bc7b
Nathan Bubna <nathan@esha.com>
2018-02-14 21:54:19 -0800

    correct any host with a proxy path, not just localhost

--- __tests__/SOAP-spec.ts
+++ __tests__/SOAP-spec.ts
@@ -36,12 +36,12 @@ test('header', () => {
 test('header shortcut', () => {
   const node = new SOAP.Header(
     'listnutrients',
-    'http://esha/soap/FoodEditService.svc'
+    'http://localhost/soap/FoodEditService.svc'
   );
   expect(node.toString()).toBe(
     '<soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">' +
       '<wsa:Action>http://ns.esha.com/2013/genesisapi/listnutrients</wsa:Action>' +
-      '<wsa:To>http://esha/soap/FoodEditService.svc</wsa:To>' +
+      '<wsa:To>http://genesis.esha.com/soap/FoodEditService.svc</wsa:To>' +
       '</soap:Header>'
   );
 });
@@ -49,14 +49,14 @@ test('header shortcut', () => {
 test('genesis request', () => {
   const node = new SOAP.Request(
     'listnutrients',
-    'http://esha/soap/FoodQueryService.svc',
+    'http://localhost/soap/FoodQueryService.svc',
     'gen'
   );
   expect(node.toString()).toBe(
     '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:gen="http://ns.esha.com/2013/genesisapi">' +
       '<soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">' +
       '<wsa:Action>http://ns.esha.com/2013/genesisapi/listnutrients</wsa:Action>' +
-      '<wsa:To>http://esha/soap/FoodQueryService.svc</wsa:To>' +
+      '<wsa:To>http://genesis.esha.com/soap/FoodQueryService.svc</wsa:To>' +
       '</soap:Header>' +
       '<soap:Body></soap:Body>' +
       '</soap:Envelope>'
